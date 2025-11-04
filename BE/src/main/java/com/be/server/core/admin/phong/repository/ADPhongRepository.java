384200570c26b5372dc2b312523c7c9d4e2759ef
NgoVietHoang21 <nt0130207458@gmail.com>
2025-11-04 17:12:44 +0700

    chuyển enum thành số#1

--- BE/src/main/java/com/be/server/core/admin/phong/repository/ADPhongRepository.java
+++ BE/src/main/java/com/be/server/core/admin/phong/repository/ADPhongRepository.java
@@ -21,7 +21,7 @@ public interface ADPhongRepository extends PhongRepository {
             SELECT p FROM Phong p 
             LEFT JOIN FETCH p.bangGia
             WHERE 
-            p.status = com.be.server.infrastructure.constant.EntityStatus.ACTIVE AND
+            p.status = 0 AND
             (:q IS NULL OR :q = '' OR p.maPhong LIKE %:q% OR p.tenPhong LIKE %:q%) AND 
             (:loaiPhong IS NULL OR p.loaiPhong = :loaiPhong) AND 
             (:trangThaiPhong IS NULL OR p.trangThaiPhong = :trangThaiPhong) AND 
@@ -39,9 +39,9 @@ public interface ADPhongRepository extends PhongRepository {
                                       @Param("sucChuaMax") Integer sucChuaMax,
                                       Pageable pageable);
 
-    @Query("SELECT p FROM Phong p LEFT JOIN FETCH p.bangGia WHERE p.status = com.be.server.infrastructure.constant.EntityStatus.ACTIVE")
+    @Query("SELECT p FROM Phong p LEFT JOIN FETCH p.bangGia WHERE p.status = 0")
     Page<PhongProjection> findAllProjection(Pageable pageable);
 
-    @Query("SELECT p FROM Phong p LEFT JOIN FETCH p.bangGia WHERE p.id = :id AND p.status = com.be.server.infrastructure.constant.EntityStatus.ACTIVE")
+    @Query("SELECT p FROM Phong p LEFT JOIN FETCH p.bangGia WHERE p.id = :id AND p.status = 0")
     Optional<PhongProjection> findByIdProjection(@Param("id") String id);
 }
