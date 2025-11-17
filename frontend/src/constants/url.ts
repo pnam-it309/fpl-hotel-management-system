344bf07642a759d1a7987b4dbee9b230e6871414
thachvietha <thachvietha09092006@gmail.com>
2025-11-17 23:37:11 +0700

    Hoàn thành màn tag

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -7,3 +7,4 @@ export const API_LE_TAN_PHONG = `${API_URL}/leTan/phong` as string
 export const API_LE_TAN_LOAI_PHONG = `${API_URL}/leTan/loai-phong` as string
 export const API_LE_TAN_DAT_PHONG = `${API_URL}/leTan/dat-phong` as string
 export const API_LE_TAN_SO_DO_PHONG = `${API_URL}/leTan/so-do-phong` as string
+export const API_LE_TAN_TAG= `${API_URL}/leTan/tag` as string
