ee43fd6622d81faeacfe606c601260cfc6668328
thachvietha <thachvietha09092006@gmail.com>
2025-11-17 23:37:11 +0700

    Hoàn thành màn tag

---
+++ backend/src/main/java/com/be/server/core/admin/tag/model/request/ADAddAndUpdateTagRequest.java
@@ -0,0 +1,16 @@
+package com.be.server.core.admin.tag.model.request;
+
+import com.be.server.core.common.base.PageableRequest;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ADAddAndUpdateTagRequest extends PageableRequest {
+    String ten;
+    String moTa;
+}
