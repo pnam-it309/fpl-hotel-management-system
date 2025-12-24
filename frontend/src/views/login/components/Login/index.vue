16ce507dded6ee1f3d2afe77f958307fc094316a
NgoVietHoang21 <nt0130207458@gmail.com>
2025-12-24 18:51:20 +0700

    Update index.vue

--- frontend/src/views/login/components/Login/index.vue
+++ frontend/src/views/login/components/Login/index.vue
@@ -66,14 +66,14 @@ function checkUserAccount() {
 <template>
   <div>
     <n-h2 depth="3" class="text-center">
-      {{ $t('login.signInTitle') }}
+      Đăng nhập
     </n-h2>
     <n-form ref="formRef" :rules="rules" :model="formValue" :show-label="false" size="large">
-      <n-form-item path="email">
-        <n-input v-model:value="formValue.email" clearable :placeholder="$t('login.accountPlaceholder')" />
+      <n-form-item path="account">
+        <n-input v-model:value="formValue.account" clearable placeholder="Tên đăng nhập / Email" />
       </n-form-item>
       <n-form-item path="pwd">
-        <n-input v-model:value="formValue.pwd" type="password" :placeholder="$t('login.passwordPlaceholder')" clearable
+        <n-input v-model:value="formValue.pwd" type="password" placeholder="Mật khẩu" clearable
           show-password-on="click">
           <template #password-invisible-icon>
             <icon-park-outline-preview-close-one />
@@ -86,25 +86,25 @@ function checkUserAccount() {
       <n-space vertical :size="20">
         <div class="flex-y-center justify-between">
           <n-checkbox v-model:checked="isRemember">
-            {{ $t('login.rememberMe') }}
+            Ghi nhớ tôi
           </n-checkbox>
           <n-button type="primary" text @click="toOtherForm('resetPwd')">
-            {{ $t('login.forgotPassword') }}
+            Quên mật khẩu?
           </n-button>
         </div>
         <n-button block type="primary" size="large" :loading="isLoading" :disabled="isLoading" @click="handleLogin">
-          {{ $t('login.signIn') }}
+          Đăng nhập
         </n-button>
         <n-flex>
-          <n-text>{{ $t('login.noAccountText') }}</n-text>
+          <n-text>Bạn chưa có tài khoản?</n-text>
           <n-button type="primary" text @click="toOtherForm('register')">
-            {{ $t('login.signUp') }}
+            Đăng ký
           </n-button>
         </n-flex>
       </n-space>
     </n-form>
     <n-divider>
-      <span op-80>{{ $t('login.or') }}</span>
+      <span op-80>Hoặc</span>
     </n-divider>
     <n-space justify="center">
       <n-button circle>
